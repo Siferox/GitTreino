4c9c9e6f3a356c40433e8814c41ce4bfdc25c191
Siferox <106772220+Siferox@users.noreply.github.com>
2023-06-19 19:05:38 +0000

    Mudança de nome de pasta

--- Terminal/bash_scripts/our_scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir /workspaces/GitTreino/Terminal/bash_scripts/magic
-cd /workspaces/GitTreino/Terminal/bash_scripts/magic
-touch file{1..100}
-ls -lh /workspaces/GitTreino/Terminal/bash_scripts/magic > /workspaces/GitTreino/Terminal/bash_scripts/magic.log
