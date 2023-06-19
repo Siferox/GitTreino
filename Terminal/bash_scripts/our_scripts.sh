d9e063ee283c9ee141da3101de9c76d526a5ef1f
Siferox <106772220+Siferox@users.noreply.github.com>
2023-06-19 19:03:18 +0000

    Teste de bash scripts

---
+++ Terminal/bash_scripts/our_scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir /workspaces/GitTreino/Terminal/bash_scripts/magic
+cd /workspaces/GitTreino/Terminal/bash_scripts/magic
+touch file{1..100}
+ls -lh /workspaces/GitTreino/Terminal/bash_scripts/magic > /workspaces/GitTreino/Terminal/bash_scripts/magic.log
