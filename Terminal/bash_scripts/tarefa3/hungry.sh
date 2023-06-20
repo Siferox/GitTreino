f025e34ef9e2051b1bd29d7b7afad19332507b57
Siferox <106772220+Siferox@users.noreply.github.com>
2023-06-20 17:10:07 +0000

    colocando a tarefa 3 no repositório

---
+++ Terminal/bash_scripts/tarefa3/hungry.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "I am hungry. Feed me data." >> /workspaces/GitTreino/Terminal/bash_scripts/tarefa3/demands.txt | date 1>> /workspaces/GitTreino/Terminal/bash_scripts/tarefa3/demands.log | echo "The monster has feeded at" 1>> /workspaces/GitTreino/Terminal/bash_scripts/tarefa3/demands.log
