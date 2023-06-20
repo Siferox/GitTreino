26d1b689131f7433feb502f2e14051d9be60bbf4
Siferox <106772220+Siferox@users.noreply.github.com>
2023-06-20 17:21:30 +0000

    Finalizando a primeira parte da tarefa 3

---
+++ Terminal/bash_scripts/magic/our_scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /workspaces/GitTreino/Terminal/bash_scripts/magic
+touch file{1..10}
+ls -lh /workspaces/GitTreino/Terminal/bash_scripts/magic > /workspaces/GitTreino/Terminal/bash_scripts/magic/magic.log
