d38b7b9df43f19bd0c23cf83fd53dad4af948938
Siferox <106772220+Siferox@users.noreply.github.com>
2023-12-21 12:45:27 +0000

    Comecei de nada com o C

--- Help/Terminal/bash_scripts/tarefa3/hungry.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "I am hungry. Feed me data." >> /workspaces/GitTreino/Terminal/bash_scripts/tarefa3/demands.txt | date 1>> /workspaces/GitTreino/Terminal/bash_scripts/tarefa3/demands.log | echo "The monster has feeded at" 1>> /workspaces/GitTreino/Terminal/bash_scripts/tarefa3/demands.log
