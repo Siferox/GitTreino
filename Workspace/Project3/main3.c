73e882ae392cf10d060c0f7fbeb72c21c5ddfb8e
Siferox <106772220+Siferox@users.noreply.github.com>
2023-12-22 14:53:28 +0000

    Estudando linguagem C, usando comentarios

--- Workspace/Project3/main3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) 
-{
-    int total;
-    
-    printf("Hello World\n");
-    return 0;
-}
--- Workspace/testProject/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hi, my name is Vitor\n");
-    return 0;
-}
