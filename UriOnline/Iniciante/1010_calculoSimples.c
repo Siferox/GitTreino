3a1b0a78c960570e520a51a02a0fd3028d49601f
Siferox <106772220+Siferox@users.noreply.github.com>
2024-01-02 14:40:29 +0000

    Não consegui fazer o 1013 do UriOnline

---
+++ UriOnline/Iniciante/1010_calculoSimples.c
@@ -0,0 +1,44 @@
+/*
+
+Cálculo Simples
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
+
+Exemplos de Entrada	Exemplos de Saída
+12 1 5.30
+16 2 5.10
+
+VALOR A PAGAR: R$ 15.50
+
+13 2 15.30
+161 4 5.20
+
+VALOR A PAGAR: R$ 51.40
+
+1 1 15.10
+2 1 15.10
+
+VALOR A PAGAR: R$ 30.20
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int codigo1, peca1, codigo2, peca2;
+    float valorunitario1, valorunitario2;
+
+    scanf("%d %d %f %d %d %f", &codigo1, &peca1, &valorunitario1, &codigo2, &peca2, &valorunitario2);
+    printf("VALOR A PAGAR: R$ %0.2f\n", peca1*valorunitario1+peca2*valorunitario2);
+
+    return 0;
+}
