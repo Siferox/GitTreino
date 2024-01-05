08a838ce2d280059a6e46d65cff16d548a530408
Siferox <106772220+Siferox@users.noreply.github.com>
2024-01-05 17:12:01 +0000

    programa de grosspay e taxes

--- C_Programming_For_Beginners_Udemy/07_Control_Flow/weeklyPay.c
+++ C_Programming_For_Beginners_Udemy/07_Control_Flow/weeklyPay.c
@@ -7,23 +7,46 @@
 int main(void)
 {
     
-    float overtime, grossPay;
+    float hours, overtime, grossPay, t15, t20, t25;
     
-    printf("inform the number os hours worked in this week: \n");
-    scanf("%f", &overtime);
-    grossPay = payRate*overtime;
-
-    if (grossPay >= 300)
-        
-        printf("The gross pay is $%0.2f, and taxes is 15 percent and the value is $%0.2f, and the net pay is $%0.2f \n", grossPay, grossPay*taxRate15, grossPay-(grossPay*taxRate15));
-
-        else if ((grossPay >= 150) && (grossPay < 300))
-        printf("The gross pay is $%0.2f, and taxes is 20 percent and the value is $%0.2f, and the net pay is $%0.2f \n", grossPay, grossPay*taxRate20, grossPay-(grossPay*taxRate20));
-
+    printf("Inform the number os hours worked in this week: \n");
+    scanf("%f", &hours);
+    
+    if (hours <= 40)
+    {
+        grossPay = payRate * hours;
+    }
         else
-        printf("The gross pay is $%0.2f, and taxes is 25 percent and the value is $%0.2f, and the net pay is $%0.2f \n", grossPay, grossPay*taxRate25, grossPay-(grossPay*taxRate25));
-    
+        {    
+        overtime = (hours - 40) * payRate * 1.5;  
+        grossPay = overtime + (40*payRate);              
+        }    
+     
+    if (grossPay < 300)
+        {
+            t15 = grossPay * taxRate15;
+            printf("The gross pay is $%0.2f\n", grossPay);
+            printf("The taxes is 15 percent and the value is $%0.2f\n", t15);
+            printf("The net pay is $%0.2f \n", grossPay-t15);
+        }
+        else if ((grossPay >= 300) && (grossPay < 450))
+        {
+            t15 = 300 * taxRate15;
+            t20 = (grossPay - 300) * taxRate20;
+            printf("The gross pay is $%0.2f\n", grossPay);
+            printf("The taxes is 20 percent and the value is $%0.2f\n", t15+t20);
+            printf("The net pay is $%0.2f \n", grossPay-(t15+t20));
+        }
+        else if (grossPay > 450)
+        {
+            t15 = 300 * taxRate15;
+            t20 = 150 * taxRate20;
+            t25 = (grossPay - 450) * taxRate25;
+            printf("The gross pay is $%0.2f\n", grossPay);
+            printf("The taxes is 25 percent and the value is $%0.2f\n", t15 + t20 + t25);
+            printf("The net pay is $%0.2f \n", grossPay-(t15 + t20 + t25));
+        }
 
-return 0;
+    return 0;
 
 }
